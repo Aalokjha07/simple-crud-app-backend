f56a54f4d65553329ec1df87385bf7cf884d77ea
aalokjha07 <aalokjha2018@gmail.com>
2025-12-14 20:22:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const Product =  require('./models/product.model.js');
+const ProductRoute = require('./routes/product.route.js');
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use('/api/products' , ProductRoute);
+
+
+app.get('/', (req , res) =>{
+    res.send('hello from node api server');
+})
+
+mongoose.connect('mongodb+srv://admin:mwVWwT6SNS7QV5Tj@db.uoqna3k.mongodb.net/NODE-API?appName=DB')
+.then(()=>{
+    console.log('connected to database..!')
+    app.listen(3000, ()=>{
+    console.log('server is running on port 3000');
+})
+})
+.catch(()=>{
+    console.log('connection failed...!s')
+})
+ 
